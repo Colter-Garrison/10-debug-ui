ccba3d779df39ce18422a9ac7c4e0dfd3764d69e
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-11-12 21:51:48 -0800

    added a debugger with x, y, z controlls

--- src/script.js
+++ src/script.js
@@ -2,6 +2,12 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import * as dat from 'dat.gui'
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI()
 
 /**
  * Base
@@ -19,6 +25,11 @@ const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+//Debug
+gui.add(mesh.position, 'x', -3, 3, 0.01)
+gui.add(mesh.position, 'y', -3, 3, 0.01)
+gui.add(mesh.position, 'z', -3, 3, 0.01)
 
 /**
  * Sizes
